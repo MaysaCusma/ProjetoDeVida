65ddd990311135cddce0f8ea979e4218e15f868b
MaysaCusma <146853333+MaysaCusma@users.noreply.github.com>
2025-04-09 16:38:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,45 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
+//Define os objetivos de tempo para os contadores
+const tempoObjetivo1 = new Date("2026-11-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-11-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-11-05T00:00:00");
+const tempoObjetivo4 = new Date("2026-11-05T00:00:00");
 
-let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+//Função que calcula o tempo restante até a data do objetivo
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date(); //Tempo atual
+    let tempoFinal = tempoObjetivo - tempoAtual; //Calcula a diferença em milissegundos
+
+    //Converter tempo restante para dias, horas, minutos e segundos
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else{
+        return "Prazo finalizado";
+    }
+}
+
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
